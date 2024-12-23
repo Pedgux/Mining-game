639a8594948276929834086f63345b73562cc818
Pedgux <matiaslehtinen08@gmail.com>
2024-12-23 15:13:14 +0200

    Initial commit of mining game project

---
+++ main.js
@@ -0,0 +1,7 @@
+const worldCanvas = /** @type {HTMLCanvasElement} */ (
+	document.getElementById("worldCanvas")
+);
+
+const ctx = worldCanvas.getContext("2d");
+ctx.fillRect(10, 10, 10, 10);
+let list = [123, 12398, 127, 123];

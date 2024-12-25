48ae69cac62cbc2cc27dd7db01381bf9715f625c
Pedgux <matiaslehtinen08@gmail.com>
2024-12-25 22:02:39 +0200

    Perkele pelaaja

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const worldCanvas = /** @type {HTMLCanvasElement} */ (
   document.getElementById("worldCanvas")
 );
 const ctx = worldCanvas.getContext("2d");
-const size = 16; //cell size, universal for the graphics
+const size = 16; //cell size
+let timeStamp = 0;
 
 function clearScreen(color) {
   ctx.fillStyle = color;
@@ -16,13 +17,17 @@ function resizeCanvas() {
 window.addEventListener("resize", resizeCanvas);
 
 function createGrid() {
-  let x = Math.floor(worldCanvas.width / size);
-  let y = Math.floor(worldCanvas.height / size);
+  let rows = Math.floor(worldCanvas.height / size);
+  let cols = Math.floor(worldCanvas.width / size);
   const grid = [];
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < rows; i++) {
     const row = [];
-    for (let j = 0; j < y; j++) {
-      row.push(0);
+    for (let j = 0; j < cols; j++) {
+      if (i > rows - 10) {
+        row.push(1);
+      } else {
+        row.push(0);
+      }
     }
     grid.push(row);
   }
@@ -31,14 +36,88 @@ function createGrid() {
 
 function drawGrid(grid) {
   clearScreen("black");
-  for (let x = 0; x < grid.length; x++) {
-    for (let y = 0; y < grid[0].length; y++) {
-      ctx.fillStyle = "blue";
-      ctx.fillRect(x * size, y * size, size, size);
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      switch (grid[i][j]) {
+        case 1:
+          ctx.strokeStyle = "white";
+          break;
+        default:
+          ctx.strokeStyle = "black";
+          break;
+      }
+      ctx.strokeRect(j * size, i * size, size, size);
+      ctx.rect;
     }
   }
-  requestAnimationFrame(() => drawGrid(grid));
 }
+
+function gameLoop(timeStamp) {
+  //update grid when kun tehty
+  player.update();
+
+  drawGrid(grid);
+  player.draw();
+  requestAnimationFrame(() => gameLoop());
+}
+
 resizeCanvas();
 let grid = createGrid();
-drawGrid(grid);
+
+let player = {
+  x: worldCanvas.width / 2,
+  y: worldCanvas.height / 2,
+  vx: 0,
+  vy: 0,
+  width: size,
+  height: size,
+  color: "white",
+
+  update: function () {
+    this.x += this.vx;
+    this.y += this.vy;
+  },
+  draw: function () {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+};
+//self explanatory 1
+function handleKeydown(event) {
+  switch (event.key) {
+    case "w":
+      player.vy = -1;
+      break;
+    case "s":
+      player.vy = 1;
+      break;
+    case "a":
+      player.vx = -1;
+      break;
+    case "d":
+      player.vx = 1;
+      break;
+  }
+}
+//self explanatory 2
+function handleKeyup(event) {
+  switch (event.key) {
+    case "w":
+      player.vy = 0;
+      break;
+    case "s":
+      player.vy = 0;
+      break;
+    case "a":
+      player.vx = 0;
+      break;
+    case "d":
+      player.vx = 0;
+      break;
+  }
+}
+
+//start the game
+window.addEventListener("keydown", handleKeydown);
+window.addEventListener("keyup", handleKeyup);
+gameLoop();
